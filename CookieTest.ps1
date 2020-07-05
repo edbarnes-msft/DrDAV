7697d69b94b2cdb649957b5330a4e050a4b565a0
Ed Barnes <38018545+edbarnes-msft@users.noreply.github.com>
2020-07-05 00:01:09 -0500

    Add files via upload
    Test for shared, persistent cookies

---
+++ CookieTest.ps1
@@ -0,0 +1,75 @@
+﻿cls
+$deftesturl =  'https://www.myserver.com'
+
+[void][System.Reflection.Assembly]::LoadWithPartialName('Microsoft.VisualBasic')
+[uri] $WebAddress = [Microsoft.VisualBasic.Interaction]::InputBox("Enter the target web folder", "Web Address", $deftesturl)
+
+$logpath = $env:TEMP+"\_CookieTest"
+New-Item -ItemType Directory -Force -Path $logpath > $null
+$logfile = $logpath +"\"+$env:COMPUTERNAME+"_"+(Get-Date -Format yyddMMhhmm)+".log"
+
+function Write-ToLog()
+{   param( $msg = "`n" )
+    Write-Host $msg
+    Add-Content $logfile -Value $msg
+}
+
+$protectmodenotenabled = ""
+
+$WinInetCookieSrc = @'
+    [DllImport("ieframe.dll", CharSet = CharSet.Auto)]
+    static extern int IEIsProtectedModeURL(string pszUrl);   
+    public static int GetProtectedMode(string url)
+    {
+        return IEIsProtectedModeURL(url);
+    }
+    [DllImport("C:\\Windows\\System32\\wininet.dll", CharSet=CharSet.Auto, SetLastError=true)]
+    static extern bool InternetSetCookie(string lpszUrl, string lpszCookieName, string lpszCookieData);
+    public static bool SetCookieString(string url, string name, string value)
+    {
+        if (!InternetSetCookie(url, name, value)) { Console.Write( "Failed to set cookie. Error code: {0}. ", Marshal.GetLastWin32Error()); return false; }
+        return true;
+    }
+
+    [DllImport("C:\\Windows\\System32\\wininet.dll", CharSet=CharSet.Auto, SetLastError=true)]
+    static extern bool InternetGetCookieEx(string pchURL, string pchCookieName, System.Text.StringBuilder pchCookieData, ref System.UInt32 pcchCookieData, int dwFlags, IntPtr lpReserved);
+    const int ERROR_NO_MORE_ITEMS = 259;
+    const int flags = 0x00003000; // INTERNET_COOKIE_NON_SCRIPT  0x00001000, INTERNET_COOKIE_HTTPONLY 0x00002000
+    public static string GetCookieString(string url)
+    {
+        // Determine the size of the cookie      
+        UInt32 datasize = 256*1024; int iResult = 0; 
+        System.Text.StringBuilder cookieData = new System.Text.StringBuilder(Convert.ToInt32(datasize));
+        if (!InternetGetCookieEx(url, null, cookieData, ref datasize, flags, IntPtr.Zero)) {
+            iResult = Marshal.GetLastWin32Error(); // Console.Write( "The request returned error {0}. ", iResult);
+            if ((ERROR_NO_MORE_ITEMS == iResult) | (datasize < 0)) return null; // Console.Write( "Datasize {0}. ", datasize);
+            // Allocate stringbuilder large enough to hold the cookie    
+            cookieData = new System.Text.StringBuilder(Convert.ToInt32(datasize));
+            if (!InternetGetCookieEx(url, null, cookieData, ref datasize, flags, IntPtr.Zero)) { Console.Write( "GetCookie request returned error {0}. ", iResult);return null;}
+        }
+        return cookieData.ToString();
+    }
+
+'@ 
+
+Add-Type -MemberDefinition $WinInetCookieSrc -Namespace WinInetCookie -Name WinAPI
+
+Write-ToLog ("Cookie test for: $WebAddress`r`n")
+if ([WinInetCookie.WinAPI]::GetProtectedMode($WebAddress) ) { $protectmodenotenabled = " not" }
+Write-ToLog ("Protect Mode is$protectmodenotenabled enabled`r`n")
+Write-ToLog ("Persistent Cookies:`n================")
+
+# Read cookies        
+$cookies = [WinInetCookie.WinAPI]::GetCookieString($WebAddress)
+if ($cookies.length ) {
+    $cookies.split("; ") | ForEach-Object{ 
+         $cookie = $_
+         #if ($cookie.Length -gt 7){ if ($cookie.Substring(0,7) -eq "FedAuth") {$fedauth = $cookie.Substring(8)}  }
+         Write-ToLog $cookie
+    }
+} else { Write-ToLog "No persistent cookies found" }
+
+ 
+[WinInetCookie.WinAPI]::SetCookieString($WebAddress, "CookieTest", "$(Get-Date -Format 'yy-dd-MM_hh:mm:ss')" )
+
+Start-Process ($env:windir + "\explorer.exe")  -ArgumentList $((Get-ChildItem $logfile).DirectoryName) 
